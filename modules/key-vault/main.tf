58275c01e7dae94c30fb3cc55cb51ded051831ba
Jammy-1 <jplested132@gmail.com>
2026-01-14 20:24:38 +0000

    - Key Vault

---
+++ modules/key-vault/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault" "main" {
+  name                       = var.key_vault_name
+  resource_group_name        = var.resource_group_name
+  location                   = var.location
+  enabled_for_disk_encryption = true
+  tenant_id                  = var.tenant_id
+  sku_name                   = "standard"
+  purge_protection_enabled   = true
+  soft_delete_retention_days = 90
+}
+
+resource "azurerm_key_vault_secret" "speech_key" {
+  name         = var.speech_key_name
+  value        = var.speech_key
+  key_vault_id = azurerm_key_vault.main.id
+}
---
+++ modules/key-vault/variables.tf
@@ -0,0 +1,11 @@
+# General
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+
+# Key Vault
+variable "key_vault_name" { type = string }
+variable "tenant_id" { type = string }
+
+# Speech Key 
+variable "speech_key_name" { type = string }
+variable "speech_key" { type = string }
