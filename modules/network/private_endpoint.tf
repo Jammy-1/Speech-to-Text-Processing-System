40e5177961db3f441b8b0dcd0af0eeef097ee46d
Jammy-1 <jplested132@gmail.com>
2026-01-16 20:54:16 +0000

    - Network; Private Endpoint

---
+++ modules/network/private_endpoint.tf
@@ -0,0 +1,21 @@
+# Storage Private Endpoint
+resource "azurerm_private_endpoint" "storage_pe" {
+  name                = var.private_endpoint_name_storage_pe
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = azurerm_subnet.aks.id
+
+  private_service_connection {
+    name                           = "storage-connection"
+    is_manual_connection           = false
+    private_connection_resource_id = var.storage_account_id
+    subresource_names              = ["blob"]
+  }
+}
+
+# Private DNS - Storage
+resource "azurerm_private_dns_zone" "storage_dns" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = var.resource_group_name
+}
+
