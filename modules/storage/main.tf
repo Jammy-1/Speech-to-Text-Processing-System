bc3bca25e4d5cafc759a44378d93b4be87a9780a
Jammy-1 <jplested132@gmail.com>
2026-01-17 14:46:25 +0000

    - Storage; Tags

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -2,6 +2,7 @@ resource "azurerm_storage_account" "main" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
+  tags                     = var.tags
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,6 +1,7 @@
 # General
 variable "resource_group_name" { type = string }
 variable "location" { type = string }
+variable "tags" { type = map(string) }
 
 # Storage 
 variable "storage_account_name" { type = string }
