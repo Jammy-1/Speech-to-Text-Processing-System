a9cfa1925ca75515f01ec45dc69515272938f8cf
Jammy-1 <jplested132@gmail.com>
2026-01-22 18:31:25 +0000

    - Queue; Extra Arguments

--- modules/queue/main.tf
+++ modules/queue/main.tf
@@ -13,16 +13,30 @@ resource "azurerm_servicebus_namespace" "service_bus" {
 resource "azurerm_servicebus_queue" "speech_queue" {
   name                         = "speech-queue"
   namespace_id                 = azurerm_servicebus_namespace.service_bus.id
+
   max_size_in_megabytes        = 1024
+  max_delivery_count = 5
+
   partitioning_enabled         = true
   requires_duplicate_detection = true
+
+  lock_duration = "PT5M"
+  default_message_ttl = "P7D"
+  dead_lettering_on_message_expiration = true
 }
 
 # Queue - Search
 resource "azurerm_servicebus_queue" "search_queue" {
   name                         = "search-queue"
   namespace_id                 = azurerm_servicebus_namespace.service_bus.id
+
   max_size_in_megabytes        = 1024
+  max_delivery_count = 5
+
   partitioning_enabled         = true
   requires_duplicate_detection = true
+
+  lock_duration = "PT5M"
+  default_message_ttl = "P7D"
+  dead_lettering_on_message_expiration = true
 }
