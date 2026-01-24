ad1d53ca6564f43f28f4f6017105a2a5a2f1712e
Jammy-1 <jplested132@gmail.com>
2026-01-24 20:14:11 +0000

    - AKS; Outputs For K8's Provider

--- modules/aks/output.tf
+++ modules/aks/output.tf
@@ -1,3 +1,21 @@
 # AKS
 output "aks_cluster_name" { value = azurerm_kubernetes_cluster.main.name }
 output "aks_principal_id" { value = azurerm_kubernetes_cluster.main.identity[0].principal_id }
+
+# Outputs For K8's Provider
+output "kube_host" { value = azurerm_kubernetes_cluster.main.kube_admin_config[0].host }
+
+output "kube_client_certificate" { 
+    value = azurerm_kubernetes_cluster.main.kube_admin_config[0].client_certificate
+    sensitive = true
+}
+
+output "kube_client_key" {
+  value     = azurerm_kubernetes_cluster.main.kube_admin_config[0].client_key
+  sensitive = true
+}
+
+output "kube_cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.main.kube_admin_config[0].cluster_ca_certificate
+  sensitive = true
+}
