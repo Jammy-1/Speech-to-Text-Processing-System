36fa2bccc64b9c3158d2852eda72336b68a7cd80
Jammy-1 <jplested132@gmail.com>
2026-01-25 19:19:30 +0000

    - Kubernetes; Provider

---
+++ modules/kubernetes/k8_provider.tf
@@ -0,0 +1,7 @@
+# K8s Provider
+provider "kubernetes" {
+  host                   = var.kube_host
+  client_certificate     = base64decode(var.kube_client_certificate)
+  client_key             = base64decode(var.kube_client_key)
+  cluster_ca_certificate = base64decode(var.kube_cluster_ca_certificate)
+}
