7d51bfab65bd681adda897289927958f79cd5e33
Jammy-1 <jplested132@gmail.com>
2026-01-26 19:49:30 +0000

    - AKS; TF FMT

--- modules/aks/output.tf
+++ modules/aks/output.tf
@@ -5,9 +5,9 @@ output "aks_principal_id" { value = azurerm_kubernetes_cluster.main.identity[0].
 # Outputs For K8's Provider
 output "kube_host" { value = azurerm_kubernetes_cluster.main.kube_admin_config[0].host }
 
-output "kube_client_certificate" { 
-    value = azurerm_kubernetes_cluster.main.kube_admin_config[0].client_certificate
-    sensitive = true
+output "kube_client_certificate" {
+  value     = azurerm_kubernetes_cluster.main.kube_admin_config[0].client_certificate
+  sensitive = true
 }
 
 output "kube_client_key" {
