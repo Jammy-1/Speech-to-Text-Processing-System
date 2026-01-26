6fdf766a5ffafec72700dddc64a791ef4fe7aa47
Jammy-1 <jplested132@gmail.com>
2026-01-26 19:42:16 +0000

    - Storage Format

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,11 +1,13 @@
 # Storage Account
 resource "azurerm_storage_account" "main" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  tags                     = var.tags
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                = var.storage_account_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
+  public_network_access_enabled = false
 }
 
 # Container - Audio 
