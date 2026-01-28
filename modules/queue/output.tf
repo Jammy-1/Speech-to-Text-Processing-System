4023ea1b821a8f96a0b618dec735e480ce2d486b
Jammy-1 <jplested132@gmail.com>
2026-01-28 19:47:14 +0000

    - Queue: Output Service Bus Namespace

--- modules/queue/output.tf
+++ modules/queue/output.tf
@@ -1,2 +1,3 @@
 # Service Bus
 output "service_bus_id" { value = azurerm_servicebus_namespace.service_bus.id }
+output "service_bus_namespace" { value = azurerm_servicebus_namespace.service_bus.name }
