46a4563389276e7259cdce624ee3875f3205cb7b
Jammy-1 <jplested132@gmail.com>
2026-01-29 18:45:14 +0000

    - Kubernetes: API RBAC, UAI & FIC  - Speech Queue

--- modules/kubernetes/api.tf
+++ modules/kubernetes/api.tf
@@ -11,6 +11,42 @@ resource "kubernetes_namespace_v1" "api" {
   }
 }
 
+# UAI - CA -  Speech Service
+resource "azurerm_user_assigned_identity" "api_uai" {
+  name                = var.uai_name_api
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+}
+
+# RBAC 
+resource "azurerm_role_assignment" "api_sb_sender_rbac" {
+  scope                = var.speech_queue_id
+  role_definition_name = "Azure Service Bus Data Sender"
+  principal_id         = azurerm_user_assigned_identity.api_uai.principal_id
+}
+
+# Service Account
+resource "kubernetes_service_account_v1" "api_sa" {
+  metadata {
+    name      = "api-sa"
+    namespace = kubernetes_namespace_v1.api.metadata[0].name
+    annotations = {
+      "azure.workload.identity/client-id" = azurerm_user_assigned_identity.api_uai.client_id
+    }
+  }
+}
+
+# FIC - API
+resource "azurerm_federated_identity_credential" "api_fic" {
+  name                = "api-fic"
+  resource_group_name = var.resource_group_name
+  parent_id           = azurerm_user_assigned_identity.api_uai.id
+  issuer              = var.aks_oidc
+  subject             = "system:serviceaccount:api:api-sa"
+  audience            = ["api://AzureADTokenExchange"]
+}
+
 # Configuration Map - Internal API
 resource "kubernetes_config_map_v1" "api_config_map" {
   metadata {
@@ -20,9 +56,8 @@ resource "kubernetes_config_map_v1" "api_config_map" {
 
   data = {
     SERVICE_BUS_NAMESPACE = "${var.service_bus_name}.servicebus.windows.net"
-    SPEECH_QUEUE_NAME     = var.speech_queue_name
-    SEARCH_QUEUE_NAME     = var.search_queue_name
-    STORAGE_QUEUE_NAME    = var.storage_queue_name
+    SPEECH_QUEUE_NAME     = var.speech_queue
+    STORAGE_QUEUE_NAME    = var.storage_queue
   }
 }
 
