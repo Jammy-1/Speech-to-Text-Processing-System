8a7ca83afc51f11301af2039d5c266e3df062772
Jammy-1 <jplested132@gmail.com>
2026-02-01 18:33:42 +0000

    - ACR; Encryption RBAC, UAI Names Adjustments

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_registry" "main" {
 
   sku                           = "Premium"
   admin_enabled                 = false
-  public_network_access_enabled = false
+  public_network_access_enabled = true
 
   identity {
     type         = "UserAssigned"
@@ -15,7 +15,7 @@ resource "azurerm_container_registry" "main" {
   }
 
   encryption {
-    key_vault_key_id   = var.key_vault_id
+    key_vault_key_id   = var.acr_encryption_key_id
     identity_client_id = azurerm_user_assigned_identity.acr_uai.client_id
   }
 }
@@ -29,11 +29,18 @@ resource "azurerm_user_assigned_identity" "acr_uai" {
 }
 
 # UAI - CI/CD
-resource "azurerm_user_assigned_identity" "ci_cd_uai" {
-  name                = var.uai_ci_cd_name
+resource "azurerm_user_assigned_identity" "ci_cd_uai_acr" {
+  name                = var.uai_ci_cd_acr_name
   resource_group_name = var.resource_group_name
   location            = var.location
   tags                = var.tags
+}
+
+# RBAC - ACR UAI - Access Encryption Key
+resource "azurerm_role_assignment" "rbac_acr_encrypt_key_access" {
+  scope                = var.acr_encryption_key_id
+  role_definition_name = "Key Vault Crypto User"
+  principal_id         = azurerm_user_assigned_identity.acr_uai.principal_id
 }
 
 # RBAC - AKS Pull ACR Container
@@ -43,9 +50,9 @@ resource "azurerm_role_assignment" "rbac_aks_acr_pull" {
   principal_id         = var.aks_uai_principal_id
 }
 
-# RBAC - CI/CD 
+# RBAC - CI/CD - ACR Push
 resource "azurerm_role_assignment" "rbac_ci_cd_acr" {
   scope                = azurerm_container_registry.main.id
   role_definition_name = "AcrPush"
-  principal_id         = azurerm_user_assigned_identity.ci_cd_uai.principal_id
+  principal_id         = azurerm_user_assigned_identity.ci_cd_uai_acr.principal_id
 }
