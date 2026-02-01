ccb3ff8cecb8e2dace78759f134dbd4977b53482
Jammy-1 <jplested132@gmail.com>
2026-02-01 18:37:46 +0000

    - ACR - Updated Variables

--- modules/acr/variables.tf
+++ modules/acr/variables.tf
@@ -6,13 +6,13 @@ variable "tags" { type = map(string) }
 # ACR
 variable "acr_name" {}
 variable "acr_encryption_key_name" { type = string }
-variable "acr_id" { type = string }
 
+# UAI
 variable "uai_acr_name" { type = string }
-variable "uai_ci_cd_name" { type = string }
+variable "uai_ci_cd_acr_name" { type = string }
 
 # AKS
 variable "aks_uai_principal_id" { type = string }
 
 # Key Vault
-variable "key_vault_id" { type = string }
+variable "acr_encryption_key_id" { type = string }
