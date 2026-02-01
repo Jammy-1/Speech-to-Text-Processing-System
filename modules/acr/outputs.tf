95b049bdc505173b076fb52dbccffb979b4bea36
Jammy-1 <jplested132@gmail.com>
2026-02-01 18:37:04 +0000

    - ACR; Outputs: UAI Client ID & Principal ID

--- modules/acr/outputs.tf
+++ modules/acr/outputs.tf
@@ -1,6 +1,21 @@
 # ACR
-output "acr_id" { value = azurerm_container_registry.main.id }
-output "acr_login_server" { value = azurerm_container_registry.main.login_server }
+output "acr_id" { 
+ value = azurerm_container_registry.main.id
+ sensitive = true
+}
 
-# UAI - CI/CD 
-output "ci_cd_uai_client_id" { value = azurerm_user_assigned_identity.ci_cd_uai.client_id }
+output "acr_login_server" { 
+ value = azurerm_container_registry.main.login_server 
+ sensitive = true 
+}
+
+# UAI  
+output "acr_uai_principal_id" {
+ value = azurerm_user_assigned_identity.acr_uai.id 
+ sensitive = true
+}
+
+output "ci_cd_uai_acr_client_id" {
+ value = azurerm_user_assigned_identity.ci_cd_uai_acr.client_id 
+ sensitive = true
+}
