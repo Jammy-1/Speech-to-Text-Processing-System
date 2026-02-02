2daba2cc8599ea042d843faa241a20272e34ffcf
Jammy-1 <jplested132@gmail.com>
2026-02-02 19:41:40 +0000

    - Cognitive; Outputs

--- modules/cognitive/output.tf
+++ modules/cognitive/output.tf
@@ -1,12 +1,27 @@
 # Speech
-output "speech_id" { value = azurerm_cognitive_account.speech_service.id }
 output "speech_name" { value = azurerm_cognitive_account.speech_service.name }
-output "speech_endpoint" { value = azurerm_cognitive_account.speech_service.endpoint }
+output "speech_id" {
+  value     = azurerm_cognitive_account.speech_service.id
+  sensitive = true
+}
+
+output "speech_endpoint" {
+  value     = azurerm_cognitive_account.speech_service.endpoint
+  sensitive = true
+}
 
 # Search
 output "search_name" { value = azurerm_search_service.search_service.name }
-output "search_endpoint" { value = "https://${azurerm_search_service.search_service.name}.search.windows.net" }
 output "transcripts_index_name" { value = azapi_resource.transcripts_index.name }
+output "search_id" {
+  value     = azurerm_search_service.search_service.id
+  sensitive = true
+}
+
+output "search_endpoint" {
+  value     = "https://${azurerm_search_service.search_service.name}.search.windows.net"
+  sensitive = true
+}
 
 # Key - Speech
 output "speech_primary_key" {
