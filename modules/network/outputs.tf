14ecfc827934580a0f3f65251fc268abfaffc6f5
Jammy-1 <jplested132@gmail.com>
2026-02-23 19:37:45 +0000

    Network; Cleaned Up Outputs & Adding Comments

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -2,24 +2,33 @@
 output "vnet_id" { value = azurerm_virtual_network.main.id }
 output "vnet_name" { value = azurerm_virtual_network.main.name }
 
-# Subnet
-output "aks_subnet_id" { value = azurerm_subnet.aks.id }
-output "ingress_subnet_id" { value = azurerm_subnet.ingress.id }
+# Subnet - Private End Point
 output "pe_subnet_id" { value = azurerm_subnet.pe.id }
-
-output "aks_subnet_name" { value = azurerm_subnet.aks.name }
-output "acr_subnet_name" { value = azurerm_subnet.acr.name }
-output "ingress_subnet_name" { value = azurerm_subnet.ingress.name }
-output "monitoring_subnet_name" { value = azurerm_subnet.monitoring.name }
-output "queue_subnet_name" { value = azurerm_subnet.queue.name }
 output "pe_subnet_name" { value = azurerm_subnet.pe.name }
-
-output "aks_subnet_address_prefixes" { value = azurerm_subnet.aks.address_prefixes }
-output "acr_subnet_address_prefixes" { value = azurerm_subnet.acr.address_prefixes }
-output "ingress_subnet_address_prefixes" { value = azurerm_subnet.ingress.address_prefixes }
-output "monitoring_subnet_address_prefixes" { value = azurerm_subnet.monitoring.address_prefixes }
-output "queue_subnet_address_prefixes" { value = azurerm_subnet.queue.address_prefixes }
 output "pe_subnet_address_prefixes" { value = azurerm_subnet.pe.address_prefixes }
+
+# Subnet - AKS
+output "aks_subnet_id" { value = azurerm_subnet.aks.id }
+output "aks_subnet_name" { value = azurerm_subnet.aks.name }
+output "aks_subnet_address_prefixes" { value = azurerm_subnet.aks.address_prefixes }
+
+# Subnet - ACR
+output "acr_subnet_name" { value = azurerm_subnet.acr.name }
+output "acr_subnet_address_prefixes" { value = azurerm_subnet.acr.address_prefixes }
+
+# Subnet - Ingress
+output "ingress_subnet_id" { value = azurerm_subnet.ingress.id }
+output "ingress_subnet_name" { value = azurerm_subnet.ingress.name }
+output "ingress_subnet_address_prefixes" { value = azurerm_subnet.ingress.address_prefixes }
+
+# Subnet - Monitoring 
+output "monitoring_subnet_name" { value = azurerm_subnet.monitoring.name }
+output "monitoring_subnet_address_prefixes" { value = azurerm_subnet.monitoring.address_prefixes }
+
+# Subnet - Queue
+output "queue_subnet_name" { value = azurerm_subnet.queue.name }
+output "queue_subnet_address_prefixes" { value = azurerm_subnet.queue.address_prefixes }
+
 
 # Public Ip 
 output "public_ip_frontend" { value = azurerm_public_ip.frontend.ip_address }
