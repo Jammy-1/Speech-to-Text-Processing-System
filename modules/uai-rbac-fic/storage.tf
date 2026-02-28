01aa646940fb90e87b4f5efe3fe94dc59bf42ac9
Jammy-1 <jplested132@gmail.com>
2026-02-28 12:27:46 +0000

    UAI-RBAC; Storage - Added  RBAC Service Bus Send/Receive Queue

--- modules/uai-rbac-fic/storage.tf
+++ modules/uai-rbac-fic/storage.tf
@@ -1,25 +1,39 @@
 # UAI & RBAC For Storage
 
-# Kubernetes - modules\storage\storage_account.tf
+# Kubernetes - modules\storage\storage_account.tf & modules\kubernetes\storage.tf
 
-# UAI - K8 -  Storage Worker
+# UAI - Storage Worker
 resource "azurerm_user_assigned_identity" "storage_worker_uai" {
   name                = var.uai_storage_worker_name
   resource_group_name = var.resource_group_name
   location            = var.location
 }
 
-# RBAC - K8 -  Storage Worker -  Audio Container  
+# RBAC - Storage Worker -  Audio Container  
 resource "azurerm_role_assignment" "rbac_storage_worker_audio_container" {
   scope                = var.audio_container_id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.storage_worker_uai.principal_id
 }
 
-# RBAC - K8 - Storage Worker - Transcripts Container  
+# RBAC - Storage Worker - Transcripts Container  
 resource "azurerm_role_assignment" "rbac_storage_worker_transcripts_container" {
   scope                = var.transcripts_container_id
   role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_user_assigned_identity.storage_worker_uai.principal_id
+}
+
+# RBAC - Storage Worker - Service Bus - Receive
+resource "azurerm_role_assignment" "rbac_storage_worker_queue_receive" {
+  scope                = var.storage_queue_id
+  role_definition_name = "Azure Service Bus Data Receiver"
+  principal_id         = azurerm_user_assigned_identity.storage_worker_uai.principal_id
+}
+
+# RBAC - Storage Worker - Service Bus - Send
+resource "azurerm_role_assignment" "rbac_storage_worker_queue_send" {
+  scope                = var.storage_queue_id
+  role_definition_name = "Azure Service Bus Data Sender"
   principal_id         = azurerm_user_assigned_identity.storage_worker_uai.principal_id
 }
 
