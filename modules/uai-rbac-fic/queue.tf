fb71c174b7acbca364d22c087191dfeee0db4af8
Jammy-1 <jplested132@gmail.com>
2026-02-28 14:26:23 +0000

    UAI-RBAC; Queue - Created Least Privledged RBAC's For All Services Connecting to UAI's

--- modules/uai-rbac-fic/queue.tf
+++ modules/uai-rbac-fic/queue.tf
@@ -1,8 +1,29 @@
 # UAI & RBAC For Queue - modules\queue
 
-# RBAC - Speech - Speech Worker UAI Link
-resource "azurerm_role_assignment" "rbac_service_bus_speech_worker" {
-  scope                = var.service_bus_id
+# RBAC - Speech Worker - Service Bus Receive 
+resource "azurerm_role_assignment" "rbac_service_bus_speech_worker_receive" {
+  scope                = var.speech_queue_id
   role_definition_name = "Azure Service Bus Data Receiver"
   principal_id         = azurerm_user_assigned_identity.speech_worker_uai.principal_id
+}
+
+# RBAC - Search Worker - Service Bus Receive 
+resource "azurerm_role_assignment" "rbac_service_bus_search_worker_receive" {
+  scope                = var.search_queue_id
+  role_definition_name = "Azure Service Bus Data Receiver"
+  principal_id         = azurerm_user_assigned_identity.search_worker_uai.principal_id
+}
+
+# RBAC - Storage Worker - Service Bus Receive 
+resource "azurerm_role_assignment" "rbac_service_bus_storage_worker_receive" {
+  scope                = var.storage_queue_id
+  role_definition_name = "Azure Service Bus Data Receiver"
+  principal_id         = azurerm_user_assigned_identity.storage_worker_uai.principal_id
+}
+
+# RBAC - API - Send
+resource "azurerm_role_assignment" "rbac_service_bus_api_send" {
+  scope                = azurerm_servicebus_namespace.service_bus.id
+  role_definition_name = "Azure Service Bus Data Sender"
+  principal_id         = azurerm_user_assigned_identity.uai_aks.principal_id
 }
