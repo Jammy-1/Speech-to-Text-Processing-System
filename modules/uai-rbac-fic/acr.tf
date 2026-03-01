ab98b8427397073c1a27ee1e34d45026f58fc42b
Jammy-1 <jplested132@gmail.com>
2026-03-01 20:16:43 +0000

    ACR; Created UAI ACR & Removed Encryption

--- modules/uai-rbac-fic/acr.tf
+++ modules/uai-rbac-fic/acr.tf
@@ -1,20 +1,9 @@
 # ACR UAI, RBAC & FIC - modules\acr\
-
-# UAI - ACR - Encryption 
-resource "azurerm_user_assigned_identity" "uai_acr_encryption" {
-  name                = var.uai_acr_encryption_name
+resource "azurerm_user_assigned_identity" "uai_acr" {
+  name                = var.uai_acr_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  tags                = var.tags
 }
-
-# RBAC - ACR UAI - Access Encryption Key
-resource "azurerm_role_assignment" "rbac_acr_encrypt_key_access" {
-  scope                = var.acr_encryption_key_id
-  role_definition_name = "Key Vault Crypto User"
-  principal_id         = azurerm_user_assigned_identity.uai_acr_encryption.principal_id
-}
-
 # UAI - CI/CD - Push Images
 resource "azurerm_user_assigned_identity" "uai_ci_cd_acr_push" {
   name                = var.uai_ci_cd_acr_name
