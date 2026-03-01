e89ceca087a46ff198fc224e02bd68b947cf0884
Jammy-1 <jplested132@gmail.com>
2026-03-01 20:25:26 +0000

    Log Analytics; Storage Monitoring

--- modules/log-analytics/log_analytics.tf
+++ modules/log-analytics/log_analytics.tf
@@ -7,3 +7,13 @@ resource "azurerm_log_analytics_workspace" "main" {
   sku               = "PerGB2018"
   retention_in_days = "30"
 }
+
+# Monitoring - Storage
+resource "azurerm_monitor_diagnostic_setting" "storage_logs" {
+  name                       = var.storage_log_name
+  target_resource_id         = var.storage_account_id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+
+  enabled_metric { category = "Capacity" }
+  enabled_metric { category = "Transaction" }
+}
--- modules/log-analytics/variables.tf
+++ modules/log-analytics/variables.tf
@@ -3,4 +3,9 @@ variable "resource_group_name" { type = string }
 variable "location" { type = string }
 variable "tags" { type = map(string) }
 
+# Log Analytics
 variable "log_workspace_name" { type = string }
+
+# Storage 
+variable "storage_log_name" { type = string }
+variable "storage_account_id" { type = string }
